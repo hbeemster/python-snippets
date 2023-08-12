67ab0039f1dd035176e3bc56be90132ee1233631
herbert <h.beemster@nki.nl>
2023-08-12 11:23:22 +0200

    - add groupby demo's

--- itertools_demo/groupby.py
+++ itertools_demo/groupby.py
@@ -1,12 +1,15 @@
 """Groupby demos."""
 import csv
+from collections import namedtuple
 from itertools import groupby
 
+from pydantic import BaseModel
 from pydantic.dataclasses import dataclass
 from pathlib import Path
 from typing import List
 
 
+# ------------------------------------------------------------------------
 @dataclass
 class Population:
     city: str
@@ -20,15 +23,19 @@ class Population:
         self.count = int("".join(i for i in self.count if i.isdigit()))
         self.year = int(self.year.split(" ")[2])
 
-
     @property
     def info(self) -> str:
         return f"{self.city} - population: {self.count}"
 
+
 # ------------------------------------------------------------------------
 def sort_and_group(iterable, *, key=None):
-    """Group sorted `iterable` on `key`."""
+    """Group sorted `iterable` on `key`.
+
+    Note can only be used is the sort key is the same as the groupby key
+    """
     return groupby(sorted(iterable, key=key), key=key)
+
 
 # ------------------------------------------------------------------------
 def load_population() -> List[Population]:
@@ -40,7 +47,9 @@ def load_population() -> List[Population]:
 # ------------------------------------------------------------------------
 def groupby_country():
     population = load_population()
-    for key, group in sort_and_group(population, key=lambda population_: population_.country):
+    for key, group in sort_and_group(
+        population, key=lambda population_: population_.country
+    ):
         print(f"{key}")
         for population_ in sorted(group, key=lambda population_: population_.city):
             print(f"  {population_.info}")
@@ -49,13 +58,87 @@ def groupby_country():
 # ------------------------------------------------------------------------
 def groupby_country_and_year():
     population = load_population()
-    for key, group in sort_and_group(population, key=lambda population_: (population_.country, population_.year)):
-        print(f"{key}")
-        for population_ in sorted(group, key=lambda population_: population_.count, reverse=True):
+    # sort by ascending country, year, and descending count
+    population = sorted(
+        population,
+        key=lambda population_: (
+            population_.country,
+            population_.year,
+            -1 * population_.count,
+        ),
+    )
+    for key, group in groupby(
+        population, key=lambda population_: (population_.country, population_.year)
+    ):
+        print(f"Country: {key[0]} - Year: {key[1]}")
+        for population_ in list(group):
+            print(f"  {population_.info}")
+
+
+# ------------------------------------------------------------------------
+def groupby_country_and_year_using_namedtuples_as_keys():
+    """Use namedtuple for better readability."""
+    population = load_population()
+    # sort by ascending country, year, and descending count
+    population = sorted(
+        population,
+        key=lambda population_: (
+            population_.country,
+            population_.year,
+            -1 * population_.count,
+        ),
+    )
+
+    GroupbyKey = namedtuple("GroupbyKey", ["country", "year"])
+    for key, group in groupby(
+        population,
+        key=lambda population_: GroupbyKey(population_.country, population_.year),
+    ):
+        print(f"Country: {key.country} - Year: {key.year}")
+        for population_ in list(group):
+            print(f"  {population_.info}")
+
+
+# ------------------------------------------------------------------------
+def groupby_country_and_year_using_dataclasses_as_keys():
+    """Use namedtuple for better readability."""
+    population = load_population()
+
+    # create a SortKey dataclass that takes care of the sorting without using the negative int sort hack
+    @dataclass(order=True)
+    class SortKey:
+        country: str
+        year: int
+        count: int
+
+
+    # sort by ascending country, year, and descending count
+    population = sorted(
+        population,
+        key=lambda population_: SortKey(
+            population_.country,
+            population_.year,
+            -1 * population_.count,
+        ),
+    )
+
+    @dataclass
+    class GroupbyKey:
+        country: str
+        year: int
+
+    for key, group in groupby(
+        population,
+        key=lambda population_: GroupbyKey(population_.country, population_.year),
+    ):
+        print(f"Country: {key.country} - Year: {key.year}")
+        for population_ in list(group):
             print(f"  {population_.info}")
 
 
 # ------------------------------------------------------------------------
 if __name__ == "__main__":
     # groupby_country()
-    groupby_country_and_year()
+    # groupby_country_and_year()
+    # groupby_country_and_year_using_namedtuples_as_keys()
+    groupby_country_and_year_using_dataclasses_as_keys()
